18b370a3f244312baa3b7600e4a61faf330ca103
emperorhan <iny27@naver.com>
2019-02-07 09:56:48 +0900

    Add hash fn

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,3 +1,5 @@
+const CryptoJS = require("crypto-js");
+
 class Block {
     constructor(index, hash, previousHash, timestamp, data) {
         this.index = index;
@@ -22,10 +24,36 @@ const getLastBlock = () => blockchain[blockchain.length - 1];
 
 const getTimestamp = () => new Date().getTime() / 1000;
 
+const createHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index, previousHash, timestamp, data).toString();
+
 const createNewBlock = data => {
     const previousBlock = getLastBlock();
     const newBlockIndex = previousBlock.index + 1;
     const newTimestamp = getTimestamp();
+    const newHash = createHash(newBlockIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(
+        newBlockIndex,
+        newHash,
+        previousBlock.hash,
+        newTimestamp,
+        data
+    )
+    return newBlock;
 };
+
+const getBlockHash = (block) => createHash(block.index, block.previousHash, block.timestamp, block.data);
+
+const isNewBlockValid = (candidateBlock, latestBlock) => {
+    if (latestBlock.index + 1 !== candidateBlock.index) {
+        console.log("The candidate block doesnt have a valid index");
+        return false;
+    } else if (latestBlock.hash !== candidateBlock.previousHash) {
+        console.log("The previousHash of the candidate block is not the hash of the latest block");
+        return false;
+    } else if (getBlockHash(candidateBlock) !== candidateBlock.hash) {
+        console.log('sdsd')
+    }
+
+}
 
 console.log(blockchain);
